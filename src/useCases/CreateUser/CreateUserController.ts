7889b67c655def7c5eb3edf4d97c57878a403c67
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-06-24 16:05:57 -0300

    fix: add await

--- src/useCases/CreateUser/CreateUserController.ts
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -6,12 +6,12 @@ export class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body
     try {
-      this.createUserCase.execute({
+      await this.createUserCase.execute({
         name,
         email,
         password,
       })
-      return response.status(201)
+      return response.status(201).send()
     } catch (err) {
       return response.status(400).json({
         message: err.message || "Unexpected error",
